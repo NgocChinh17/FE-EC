e0b3ec248a955c48abf385efddffee0c31aea972
NgocChinh17 <chinhwedit@gmail.com>
2024-10-15 10:28:44 +0700

    push fe

---
+++ src/redux/slicers/orderSlides.js
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  orderItems: [],
+  shippingAddress: {},
+  paymentMethod: "",
+  itemsPrice: 0,
+  shippingPrice: 0,
+  totalPrice: 0,
+  user: "",
+  isPaid: false,
+  paiAt: "",
+  isDelivered: false,
+  isDeliveredAt: "",
+};
+
+const orderSlice = createSlice({
+  name: "order",
+  initialState,
+  reducers: {
+    addOrderProduct: (state, action) => {
+      const { orderItem } = action.payload;
+      const itemOrder = state.orderItems.find(
+        (item) => item.Product === orderItem.Product
+      );
+
+      if (itemOrder) {
+        itemOrder.amount += orderItem.amount;
+      } else {
+        state.orderItems.push(orderItem);
+      }
+    },
+
+    updateProductAmount: (state, action) => {
+      const { idProduct, newAmount } = action.payload;
+      const itemOrder = state.orderItems.find((item) => item.Product === idProduct);
+
+      if (itemOrder) {
+        itemOrder.amount = newAmount;
+      }
+    },
+
+    removeOrderProduct: (state, action) => {
+      const { idProduct } = action.payload;
+      state.orderItems = state.orderItems.filter((item) => item.Product !== idProduct);
+    },
+
+    clearOrder: (state) => {
+      state.orderItems = [];
+    },
+  },
+});
+
+export const { addOrderProduct, removeOrderProduct, updateProductAmount, clearOrder } =
+  orderSlice.actions;
+
+export default orderSlice.reducer;
